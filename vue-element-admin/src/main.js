aa8813cfe87b1a41342c70dfd4fdc2462c19c2fa
wuys <yongsheng.wu@rtdream.cn>
2019-12-19 17:42:10 +0800

    提交admin代码

---
+++ vue-element-admin/src/main.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import i18n from './lang' // internationalization
+import './icons' // icon
+import './permission' // permission control
+import { default as api } from './utils/request'
+import { checkPermission } from './utils/permission'
+import './utils/error-log' // error log
+
+import * as filters from './filters' // global filters
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+Vue.prototype.api = api
+// 全局的常量
+Vue.prototype.hasPerm = checkPermission
+// 生产环境时自动设置为 false 以阻止 vue 在启动时生成生产提示。
+Vue.config.productionTip = (process.env.NODE_ENV !== 'production')
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  i18n,
+  render: h => h(App)
+})
---
+++ vue-element-admin/src/utils/request.js
@@ -0,0 +1,82 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
+
+axios.defaults.withCredentials = true
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+service.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    // do something before request is sent
+
+    if (store.getters.token) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
+      config.headers['X-Token'] = getToken()
+    }
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.code === '1000') {
+      return res
+    }
+    if (res.code === '200') {
+      return res
+    }
+    if (res.code === '100') {
+      return res.info
+    } else if (res.code === '20011') {
+      Message({
+        showClose: true,
+        message: res.msg,
+        type: 'error',
+        duration: 500,
+        onClose: () => {
+          store.dispatch('FedLogOut').then(() => {
+            location.reload()// 为了重新实例化vue-router对象 避免bug
+          })
+        }
+      })
+      return Promise.reject('未登录')
+    } else {
+      Message({
+        message: res.msg,
+        type: 'error',
+        duration: 3 * 1000
+      })
+      return Promise.reject(res)
+    }
+  },
+  error => {
+    console.error('err' + error)// for debug
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 3 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
